4eb87f9c951627eaa282266da05554ba08af7d75
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-14 03:30:36 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,9 +5,10 @@ index = 0
 def position_taken?(array, index)
  array.each.with_index(1) do |value, index|
    if index = "" || " "
-     return false
+     return false end 
    else
      return true
  end
 end
+
 position_taken?(board, index)
