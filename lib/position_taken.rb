74a9680c240d94e93b9f5f0fc5ea315089df7b47
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-14 03:52:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,15 +4,18 @@ index = 0
 
 def position_taken?(array, index)
  array.each.with_index(1) do |value, idx|
-   if idx = "" || " " || nil
+   if idx = "X" || "O" 
+    return true
+   puts "True"
+
+ elsif idx = "" || " " || nil
      return false
      puts "false"
-   elsif idx = "X" || "O" 
-     return true
-     puts "True"
+
    else
      return nil
    end
+   
   end
   puts array[0]
  end
