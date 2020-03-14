481334c5b98b12c31ad47b40cd9a8c581fcc44dd
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-14 04:25:40 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,12 +3,11 @@ board = ["X"]
 index = 0
 
 def position_taken?(array, index)
- array.each.with_index(0) do |value, index|
+ array.each.with_index(1) do |value, index|
    if value != "X" || "O"
      puts "false"
    else
      puts "true"
-    end
   end
 
   #puts array[0]
