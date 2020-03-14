52bbff757d23648b38d46288ed7a383397bb5a9e
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-14 04:08:49 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -6,7 +6,8 @@ def position_taken?(array, index)
  array.each.with_index(1) do |value, idx|
    if value == "X" || "O"
      return true
-   else value == " " || "" || nil
+   elsif array.each.with_index(1) do |value, idx|
+      value == " " || "" || nil
      return false
     end
   end
