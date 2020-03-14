15026ff01be80a336c65b2c6c55e8bc55532bc02
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-14 03:29:29 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,12 +2,12 @@
 board = []
 index = 0
 
-def position_taken?(board, index)
- if board[] == "" || " "
-   puts false
- else
-   puts true
- end
+def position_taken?(array, index)
+ array.each.with_index(1) do |value, index|
+   if index = "" || " "
+     return false
+   else
+     return true
 end
 
 position_taken?(board, index)
